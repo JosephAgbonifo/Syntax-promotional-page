1a9a843467ab2a7580d40747566c17eac176346d
Agbonifo Joseph <joesefair@gmail.com>
2025-01-08 18:21:29 +0100

    remove useless js codes

--- scripts/script.js
+++ scripts/script.js
@@ -7,14 +7,3 @@ function closeNav() {
     document.getElementsByClassName("control-me")[0].style.width = "50%";
     document.getElementsByClassName("menu-icon")[0].style.display = "none";
   }
-
-  // hero scroll effect
-window.addEventListener("scroll", setscrollvar);
-function setscrollvar(){
-  const htmlElement = document.documentElement;
-  const pOfScreenScroll = htmlElement.scrollTop / htmlElement.clientHeight;
-  htmlElement.style.setProperty("--scroll", Math.min(pOfScreenScroll * 100, 100));
-  console.log(Math.min(pOfScreenScroll * 100, 100))
-}
-
-setscrollvar();
