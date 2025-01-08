fdab7050948b7157e3953c404b05b675dd37bb6c
Agbonifo Joseph <joesefair@gmail.com>
2025-01-08 22:37:16 +0100

    add fade-in animation and beautified 'what to expect' section

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,25 @@ function closeNav() {
     document.getElementsByClassName("control-me")[0].style.width = "50%";
     document.getElementsByClassName("menu-icon")[0].style.display = "none";
   }
+
+const faders = document.querySelectorAll(".fade-in");
+const options = {
+  threshold: 1,
+  rootMargin: "0px 0px 0px 0px"
+};
+
+
+const appearOnScroll = new  IntersectionObserver( function(entries,appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting){
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, options)
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
